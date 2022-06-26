fd7b05a259510fc94e56566798e0e678690a4268
LindsayLew <lmschwehr@gmail.com>
2022-06-25 19:39:09 -0700

    script2 please connect

--- script2.js
+++ script2.js
@@ -1,11 +1,16 @@
 //let's try this again...
 
+
+// come back to meeeee
 function createPassword() {
-    var password = genPassword();
+    var password = genPassword(genButton);
     var passwordComp = document.querySelector("#password");
     passwordComp.value = password;
 }
 
-genButton.addEventListener("click", createPassword);
+genButton.addEventListener("click", createPassword());
+    var genButton = document.querySelector("#generate");
+    // genButton onclick ="createPassword"
+
 
 //to continue, I need to make sure I define genPassword()
